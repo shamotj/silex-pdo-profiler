5a82e59c4688d8e83329d8e8fc1582d150eadd4f
Tomáš Jenej <shamot@neonus.sk>
2016-06-03 14:53:03 +0200

    Updated for Silex 2

--- src/Shamotj/Provider/PdoProfilerServiceProvider.php
+++ src/Shamotj/Provider/PdoProfilerServiceProvider.php
@@ -2,32 +2,35 @@
 
 namespace Shamotj\Provider;
 
+use Pimple\Container;
 use Silex\Application;
-use Silex\ServiceProviderInterface;
+use Pimple\ServiceProviderInterface;
 use Shamotj\DataCollector\PdoDataCollector;
 
 class PdoProfilerServiceProvider implements ServiceProviderInterface
 {
-    public function register(Application $app)
+    public function register(Container $app)
     {
-        $dataCollectors = $app['data_collectors'];
-        $dataCollectors['db'] = $app->share(function ($app) {
+        $app->extend('data_collectors', function ($collectors, $app) {
+            $collectors['db'] = function ($app) {
 
-            $collector = new PdoDataCollector($app['pdo']);
+                $collector = new PdoDataCollector($app['pdo']);
 
-            return $collector;
+                return $collector;
+            };
+            return $collectors;
         });
-        $app['data_collectors'] = $dataCollectors;
 
-        $dataCollectorTemplates = $app['data_collector.templates'];
-        $dataCollectorTemplates[] = array('db', '@DoctrineBundle/Collector/db.html.twig');
-        $app['data_collector.templates'] = $dataCollectorTemplates;
+        $app['data_collector.templates'] = $app->extend('data_collector.templates', function ($templates) {
+            $templates[] = array('db', '@DoctrineBundle/Collector/db.html.twig');
+            return $templates;
+        });
 
-        $app['twig.loader.filesystem'] = $app->share($app->extend('twig.loader.filesystem', function ($loader) {
+        $app['twig.loader.filesystem'] = $app->extend('twig.loader.filesystem', function ($loader) {
             /** @var \Twig_Loader_Filesystem $loader */
             $loader->addPath(dirname(__DIR__).'/Resources/views', 'DoctrineBundle');
             return $loader;
-        }));
+        });
     }
 
     public function boot(Application $app)
